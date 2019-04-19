6e4ce70040af3acc390b3ddad489c5ea5877a8ff
mike <b09780978@gmail.com>
2019-04-19 22:28:50 +0800

    Use aiohttp.connector.TCPConnector to hold connect

--- Client.py
+++ Client.py
@@ -3,7 +3,8 @@
 # @Author  : faker (b09780978@gmail.com)
 # @Version : 0.0.1
 import asyncio
-from aiohttp import ClientSession
+from aiohttp import request
+from aiohttp.connector import TCPConnector
 
 class AioSessionException(Exception):
 	'''
@@ -14,7 +15,6 @@ class AioSessionException(Exception):
 
 	def __str__(self):
 		return self._error_message
-
 
 class AioSessionContext(dict):
 	'''
@@ -52,59 +52,47 @@ class HTTPResponse(dict):
 		return self.get('status', 0)
 
 class AioSession(object):
+
 	def __init__(self, config):
 		if not isinstance(config, dict):
 			raise AioSessionException('Client(config) error\nconfig must a dict')
 
-		self._context = AioSessionContext(config)
 		self._loop = asyncio.get_event_loop() if config.get('loop', None) is None else config.get('loop')
+		self._connector = TCPConnector(loop=self._loop)
+		self._context = AioSessionContext(config)
+		self._context.update({'connector' : self._connector })
+		self._closed = False
 
-	'''
-		Real request function
-	'''
 	async def _request(self, method, url, dataType, encoding, **kwargs):
-		async with ClientSession(**self._context) as session:
-			method = method.upper()
-			request_func = None
-			result = None
+		'''
+		Real request function:
+			I create a aiohttp.connector.TCPConnector by myself,
+			In each time, make a http request
+			I store cookies and headers
+		'''
+		method = method.upper()
+		result = None
+		response = None
 
-			if method == 'GET':
-				request_func = session.get
-			elif method == 'POST':
-				request_func = session.post
-			elif method == 'HEAD':
-				request_func = session.head
-			elif method == 'PUT':
-				request_func = session.put
-			elif method == 'DELETE':
-				request_func = session.delete
-			elif method == 'OPTIONS':
-				request_func = session.options
-			elif method == 'TRACE':
-				request_func == session.trace
-			elif method == 'PATCH':
-				request_func == session.patch
+		if  method not in ('GET', 'POST', 'HEAD', 'PUT', 'DELETE', 'OPTIONS', 'TRACE', 'PATCH'):
+			raise AioSessionException('Unknow HTTP method {}'.format(method))
+			
+		# Prepare args for aiohttp.request
+		request_args = self._context.copy()
+		request_args.update(kwargs)
+
+		async with request(method, url, **request_args) as resp:
+			if dataType == 'html':
+				result = await resp.text(encoding=encoding)
+			elif dataType == 'json':
+				result = await resp.json()
+			elif dataType == 'media':
+				result = await resp.read()
 			else:
-				raise AioSessionException('Unknow HTTP method {}'.format(method))
-
-			response = None
-
-			async with request_func(url, **kwargs) as resp:
-				result = None
-				if dataType == 'html':
-					result = await resp.text(encoding=encoding)
-				elif dataType == 'json':
-					result = await resp.json()
-				elif dataType == 'media':
-					result = await resp.read()
-				else:
-					raise AioSessionException('Unknow request data type: {}'.format(dataType))
-
-				self._context.update( { 'headers' : session._default_headers, 'cookies' : session._cookie_jar })
-				response = HTTPResponse(data=result, url=resp.url, status=resp.status, history=resp.history)
-
+				raise AioSessionException('Unknow request data type: {}'.format(dataType))
+				self._context.update( { 'headers' : session._default_headers, 'cookies' : session._cookie_jar } )
+			response = HTTPResponse(data=result, url=resp.url, status=resp.status, history=resp.history)
 			return response
-
 
 	'''
 		HTTP method: GET
@@ -178,10 +166,29 @@ class AioSession(object):
 	async def _patch(self, url, dataType, encoding, **kwargs):
 		return await self._request('PATCH', url, dataType, encoding, **kwargs)
 
+	async def _close_connector(self):
+		if not self.closed:
+			await self._connector.close()
+			self._connector = None
+
+	@property
+	def closed(self):
+		return self._closed
+
+	@closed.setter
+	def closed(self, new_status):
+		self._closed = new_status
+	
 	def close(self):
 		# Prevent tls close too quickly
+		self._loop.run_until_complete(self._close_connector())
 		self._loop.run_until_complete(asyncio.sleep(0.25))
 		self._loop.close()
+		self.closed = True
+
+	def __del__(self):
+		if not self.closed:
+			self.close()
 
 # Alias name
 Session = AioSession
